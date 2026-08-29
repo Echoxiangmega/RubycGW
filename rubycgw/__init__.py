d22e210d134c787def16c0a55db57967114da6d2
Echoxiangmega <130957410+Echoxiangmega@users.noreply.github.com>
2026-08-29 18:39:12 +0800

    Export noninteracting reference solver

--- rubycgw/__init__.py
+++ rubycgw/__init__.py
@@ -2,13 +2,19 @@
 
 from .model import RubyParameters, build_h0, build_interaction, eta_vertices
 from .grids import MatsubaraGrid
-from .gw import GWOptions, GWResult, solve_gw
+from .gw import (
+    GWOptions,
+    GWResult,
+    NonInteractingResult,
+    solve_gw,
+    solve_noninteracting,
+)
 from .cgw import VertexOptions, VertexResult, solve_vertex_q0
 from .susceptibility import chi_eta, channel_summary
 
 __all__ = [
     "RubyParameters", "MatsubaraGrid", "GWOptions", "GWResult",
-    "VertexOptions", "VertexResult", "build_h0", "build_interaction",
-    "eta_vertices", "solve_gw", "solve_vertex_q0", "chi_eta",
-    "channel_summary",
+    "NonInteractingResult", "VertexOptions", "VertexResult", "build_h0",
+    "build_interaction", "eta_vertices", "solve_gw", "solve_noninteracting",
+    "solve_vertex_q0", "chi_eta", "channel_summary",
 ]
