03f2bfa83876689c43d0cfc55c0562741f193b34
Echoxiangmega <130957410+Echoxiangmega@users.noreply.github.com>
2026-09-15 20:49:01 +0800

    Expose checkpoint IO

---
+++ rubycgw/io/__init__.py
@@ -0,0 +1,5 @@
+"""Input/output helpers for stable workflow objects."""
+
+from .background import FORMAT_VERSION, background_metadata, save_background
+
+__all__ = ["FORMAT_VERSION", "save_background", "background_metadata"]
